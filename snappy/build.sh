4772ab6253641027a56bb321fbfa9faf1098562a
Daniela <remenska@gmail.com>
2015-09-04 05:06:05 -0400

    snappy

--- snappy/build.sh
+++ snappy/build.sh
@@ -1,20 +1,11 @@
 echo 'gcc version' 
 gcc -v
-
-<<<<<<< HEAD
-export TMPDIR=/media/New/tmpdir
-
-
-=======
->>>>>>> 833248fe022f4794f838d5f6f396cc1aa1a2af90
-    ./configure \
+   ./configure \
         --prefix=$PREFIX \
 	--libexecdir=$PREFIX/lib \
 	--libdir=$PREFIX/lib \
 	--includedir=$PREFIX/include \
 	|| return 1;
-
-
 
 make
 make install
