9f529ed873b9ea474f448228ab2bcbc40d834b58
Daniela <remenska@gmail.com>
2015-09-07 07:39:41 -0700

    MacOS

--- gcc/build.sh
+++ gcc/build.sh
@@ -3,31 +3,38 @@ ln -s $PREFIX/lib $PREFIX/lib64
 echo 'gcc version' 
 gcc -v
 
-export TMPDIR=/media/osboxes/New/tmpdir
+#export TMPDIR=/media/osboxes/New/tmpdir
+
+./contrib/download_prerequisites
+
+cd ..
+mkdir objdir
+cd objdir
 
 if [ "$(uname)" == "Darwin" ]; then
     export LDFLAGS="-Wl,-headerpad_max_install_names"
     export BOOT_LDFLAGS="-Wl,-headerpad_max_install_names"
 
-    ./configure \
+    $PWD/../gcc-4.8.2/configure \
         --prefix=$PREFIX \
-        --libdir=$PREFIX/lib \
-        --with-gmp=$PREFIX \
-        --with-mpfr=$PREFIX \
-        --with-mpc=$PREFIX \
-        --with-isl=$PREFIX \
-        --with-cloog=$PREFIX \
-        --with-boot-ldflags=$LDFLAGS \
-        --with-stage1-ldflags=$LDFLAGS \
-        --enable-checking=release \
         --disable-multilib \
         --enable-languages=c,c++ 
+        #—libdir=$PREFIX/lib \
+        #-—with-gmp=$PREFIX \
+        #—-with-mpfr=$PREFIX \
+        #—-with-mpc=$PREFIX \
+        #-—with-isl=$PREFIX \
+        #--with-cloog=$PREFIX \
+        #--with-boot-ldflags=$LDFLAGS \
+        #--with-stage1-ldflags=$LDFLAGS \
+
 else
     # For reference during post-link.sh, record some
     # details about the OS this binary was produced with.
-    mkdir -p ${PREFIX}/share
-    cat /etc/*-release > ${PREFIX}/share/conda-gcc-build-machine-os-details
-    ./configure \
+    #mkdir -p ${PREFIX}/share
+    #cat /etc/*-release > ${PREFIX}/share/conda-gcc-build-machine-os-details
+    
+     $PWD/../gcc-4.8.2/configure \
         --prefix=$PREFIX \
         --libdir=$PREFIX/lib \
         --with-gmp=$PREFIX \
--- root5/build.sh
+++ root5/build.sh
@@ -47,7 +47,7 @@ LinuxInstallation() {
         #--enable-shared \
         #--enable-sqlite \
         #--enable-ssl \
-        #--with-qt-incdir=${PREFIX}/include/ \
+        #-	-with-qt-incdir=${PREFIX}/include/ \
         #--with-qt-libdir=${PREFIX}/lib/ \
         #--with-ssl-incdir=${PREFIX}/include/openssl/ \
         #--with-ssl-libdir=${PREFIX}/lib/ \
@@ -85,15 +85,92 @@ LinuxInstallation() {
     return 0;
 }
 
+MacInstallation() {
+    export CPPFLAGS=“-I${PREFIX}/include”
+    export CPATH=“${PREFIX}/include”
+    export LIBPATH=“${PREFIX}/lib”
+    export CMAKE_OSX_DEPLOYMENT_TARGET=
+    export MACOSX_DEPLOYMENT_TARGET=
+    export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
+    chmod +x configure;
+    export CC=clang
+
+    make distclean;
+
+  #  ./configure \
+  #      ${ARCH,,*}x8664gcc \
+  #      --minimal \
+  #      --enable-x11 \
+  #      --enable-python \
+  #      --enable-xml \
+  #      --with-python-incdir=`python3.4-config --exec-prefix`/include/python3.4m \
+  #      --with-python-libdir=`python3.4-config --exec-prefix`/lib \
+  #      --etcdir=${PREFIX}/etc/root \
+  #      --prefix=${PREFIX} \
+   #      || return 1;
+        #--enable-roofit \
+        # --enable-x11 \
+        #--enable-xml \
+        # --prefix=${PREFIX} \
+        #--enable-rpath \        
+        # --enable-soversion \
+        #--enable-qt \
+        #--enable-minuit2 \
+        #--enable-shared \
+        #--enable-sqlite \
+        #--enable-ssl \
+        #--with-qt-incdir=${PREFIX}/include/ \
+        #--with-qt-libdir=${PREFIX}/lib/ \
+        #--with-ssl-incdir=${PREFIX}/include/openssl/ \
+        #--with-ssl-libdir=${PREFIX}/lib/ \
+        #--with-ssl-shared=yes \
+        #--with-sqlite-incdir=${PREFIX}/include/  \
+        #--with-sqlite-libdir=${PREFIX}/lib/  \
+        #--with-python-incdir=${PREFIX}/include/python${PY_VER}/ \
+        #--with-python-libdir=${PREFIX}/lib/ \
+        #--with-x11-libdir=${PREFIX}/lib/ \
+        
+    mkdir workdir
+    cd workdir
+
+    cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -Dbuiltin_llvm=ON \
+    -Dbuiltin_zlib=ON \
+    -Dbuiltin_freetype=ON \
+    -Dcxx11=ON \
+    -Drpath=ON \
+    -Droofit=ON \
+    -Dopengl=OFF \
+    -Dgviz=OFF \
+    || return 1;     
+    #-DCMAKE_C_COMPILER=/usr/bin/llvm-gcc \
+    #-DCMAKE_CXX_COMPILER=/usr/bin/llvm-g++ \
+ 
+   
+    
+    #-Dbuiltin_gsl=ON \
+
+    make || return 1;
+    make install || return 1;
+
+    return 0;
+
+}
+
+
 case ${ARCH} in
     'Linux')
         LinuxInstallation || exit 1;
+        ;;
+    'Darwin')
+        MacInstallation || exit 1;
         ;;
     *)
         echo -e "Unsupported machine type: ${ARCH}";
         exit 1;
         ;;
 esac
+
 
 #POST_LINK="${PREFIX}/bin/.root-post-link.sh"
 #cp -v ${RECIPE_DIR}/post-link.sh ${POST_LINK};
