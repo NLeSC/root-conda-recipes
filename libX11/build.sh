2edf65a638b44464f203e14d8259381b248ec8ba
daniela <remenska@gmail.com>
2015-09-12 18:14:23 +0200

    preserve symlinks

--- libX11/build.sh
+++ libX11/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p $PREFIX/lib
-/bin/cp -P /opt/X11/lib/*.dylib $PREFIX/lib/
+/bin/cp -rH /opt/X11/lib/*.dylib $PREFIX/lib/
 # /bin/ln -s libX11.so.6.3.0 $PREFIX/lib/libX11.so.6
 # /bin/ln -s libX11.so.6.3.0 $PREFIX/lib/libX11.so
 
