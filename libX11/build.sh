aa8c466d43fc932580489ef5ee9cee327b42f6de
daniela <remenska@gmail.com>
2015-09-12 17:39:07 +0200

    .

---
+++ libX11/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p $PREFIX/lib
+/bin/cp /opt/X11/lib/* $PREFIX/lib/
+# /bin/ln -s libX11.so.6.3.0 $PREFIX/lib/libX11.so.6
+# /bin/ln -s libX11.so.6.3.0 $PREFIX/lib/libX11.so
+
+#mkdir -p $PREFIX/include/X11
+#/bin/cp /usr/include/X11/Xlib.h $PREFIX/include/X11
