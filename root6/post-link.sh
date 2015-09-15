1561afb0efba4ec83fd15950e7041f9023d20a95
daniela <remenska@gmail.com>
2015-09-15 13:48:04 +0200

    pre/post env for ROOT

--- root6/post-link.sh
+++ root6/post-link.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source $PREFIX/bin/thisroot.sh
 
-cat <<EOF >> ${PREFIX}/.messages.txt
-ROOT has been sourced. Environment settings are ready. 
-EOF
+echo "ROOT has been sourced. Environment settings are ready. "
+
