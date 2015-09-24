f5f7a02b96ca5a64b4d4551431a52e6063339398
daniela <remenska@gmail.com>
2015-09-24 17:11:46 +0200

    root5, env set.

---
+++ root5/pre-unlink.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Unsetting ROOT environment variables.."
+
+if [ -n "${ROOTSYS}" ]; then
+     unset ROOTSYS
+fi
+
+if [ -n "${PYTHONPATH}" ]; then
+     unset PYTHONPATH
+fi
+
+
+if [ -n "${LD_LIBRARY_PATH}" ]; then
+     unset LD_LIBRARY_PATH
+fi
+
+
+if [ -n "${DYLD_LIBRARY_PATH}" ]; then
+     unset DYLD_LIBRARY_PATH
+fi
