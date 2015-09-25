97574e0ac044837ea9ebd9a8fe05bf87ebee609e
daniela <remenska@gmail.com>
2015-09-25 12:37:10 +0200

    .

--- glibc/build.sh
+++ glibc/build.sh
@@ -1,6 +1,9 @@
 mkdir build
 
 mkdir -p ${PREFIX}/lib
+mkdir -p ${PREFIX}/include/bits
+mkdir -p ${PREFIX}/include/gnu
+
 
 cd build
 
@@ -11,3 +14,6 @@ make install-bootstrap-headers=yes install-headers
 make -j2 csu/subdir_lib
 
 install csu/crt1.o csu/crti.o csu/crtn.o ${PREFIX}/lib
+
+touch ${PREFIX}/include/bits/stdio_lim.h
+touch ${PREFIX}/include/gnu/stubs.h
