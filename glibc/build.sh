f49dfb62d66f98b7106e0cb58a3d351fc87f1793
daniela <remenska@gmail.com>
2015-09-25 12:25:00 +0200

    grr

--- glibc/build.sh
+++ glibc/build.sh
@@ -1,4 +1,7 @@
 mkdir build
+
+mkdir -p ${PREFIX}/lib
+
 cd build
 
 ../configure --prefix=$PREFIX  --build=$MACHTYPE --host=x86_64-linux-gnu --target=x86_64-linux-gnu --disable-multilib libc_cv_forced_unwind=yes
@@ -8,4 +11,3 @@ make install-bootstrap-headers=yes install-headers
 make -j2 csu/subdir_lib
 
 install csu/crt1.o csu/crti.o csu/crtn.o ${PREFIX}/lib
-
