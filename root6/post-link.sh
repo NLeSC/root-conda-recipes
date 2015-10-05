3be2e689446312b56a4995cc36453b666d917966
daniela <remenska@gmail.com>
2015-10-05 13:30:20 +0200

    MacOS DYLD_LIBRARY_PATH not needed.

--- root6/post-link.sh
+++ root6/post-link.sh
@@ -4,6 +4,16 @@ source $PREFIX/bin/thisroot.sh
 echo "ROOT has been sourced. Environment settings are ready. "
 echo "ROOTSYS="${ROOTSYS}
 
+# not needed for conda to work with ROOT
+if [ -n "${LD_LIBRARY_PATH}" ]; then
+     unset LD_LIBRARY_PATH
+fi
+
+
+if [ -n "${DYLD_LIBRARY_PATH}" ]; then
+     unset DYLD_LIBRARY_PATH
+fi
+
 
 
 #mkdir -p $PREFIX/etc/conda/activate.d
