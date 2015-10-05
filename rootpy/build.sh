837671c9e502215f79a786f68a8a525d803967f1
daniela <remenska@gmail.com>
2015-10-05 15:40:03 +0200

    .

--- rootpy/build.sh
+++ rootpy/build.sh
@@ -1,56 +1,14 @@
 #!/bin/bash
 set -x
-echo $PREFIX
-export CFLAGS="-Wall -g -m64 -pipe -O2 -march=x86-64 -fPIC"
-export CXXLAGS="${CFLAGS}"
-export CPPFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
 
-ARCH="$(uname 2>/dev/null)"
+# ROOTSYS=$(root-config --prefix)
+# pushd $ROOTSYS
+# source bin/thisroot.sh
+# popd
+# #$PYTHON -c "import ROOT"
+# echo $ROOTSYS
 
-ROOTSYS=$(root-config --prefix)
-pushd $ROOTSYS
-source bin/thisroot.sh
-popd
-#$PYTHON -c "import ROOT"
-echo $ROOTSYS
-
-LinuxInstallation() {
-
-  $PYTHON setup.py install || return 1;
-  return 0;
-  
-}
-
-MacInstallation() {
-    export CPPFLAGS="-I${PREFIX}/include"
-    export CPATH="${PREFIX}/include"
-    export LIBPATH="${PREFIX}/lib"
-    export CMAKE_OSX_DEPLOYMENT_TARGET=10.8
-    export MACOSX_DEPLOYMENT_TARGET=10.8
-    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
-    #export CC=clang
-    export CFLAGS="${CFLAGS} -D_DARWIN_SOURCE"
-    $PYTHON setup.py install || return 1;
-    return 0;
-}
-    
-    
-case ${ARCH} in
-    'Linux')
-        LinuxInstallation || exit 1;
-        ;;
-    'Darwin')
-        MacInstallation || exit 1;
-        ;;
-    *)
-        echo -e "Unsupported machine type: ${ARCH}";
-        exit 1;
-        ;;
-esac
-
-
-
+$PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
 
