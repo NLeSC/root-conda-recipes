6ad48cf74d4e4a4fd24522b133147aa2225c856b
daniela <remenska@gmail.com>
2015-10-06 18:09:11 +0200

    .

--- root6/post-link.sh
+++ root6/post-link.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 source $PREFIX/bin/thisroot.sh
 
-echo "ROOT has been sourced. Environment settings are ready. "
+echo "Postlink: ROOT has been sourced. Environment settings are ready. "
 echo "ROOTSYS="${ROOTSYS}
 
-# not needed for conda to work with ROOT
 if [ -n "${LD_LIBRARY_PATH}" ]; then
      unset LD_LIBRARY_PATH
 fi
@@ -13,13 +12,3 @@ fi
 if [ -n "${DYLD_LIBRARY_PATH}" ]; then
      unset DYLD_LIBRARY_PATH
 fi
-
-
-
-#mkdir -p $PREFIX/etc/conda/activate.d
-#mkdir -p $PREFIX/etc/conda/deactivate.d
-
-#cp ${RECIPE_DIR}/activateROOT.sh $PREFIX/etc/conda/activate.d
-#cp ${RECIPE_DIR}/deactivateROOT.sh $PREFIX/etc/conda/deactivate.d
-
-
