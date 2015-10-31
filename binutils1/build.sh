d00f797e2e87a51dff9bf71324061f693c1b2f7e
Daniela <remenska@gmail.com>
2015-10-31 19:47:54 +0100

    binutils

--- binutils1/build.sh
+++ binutils1/build.sh
@@ -1,4 +1,8 @@
-./configure --prefix=$PREFIX --disable-nls
+export CXXLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+./configure --prefix=$PREFIX --disable-nls --disable-multilib
 make
 make check
 make install
