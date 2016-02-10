763612975256c7e0c648f4aca99692634124f25b
Daniela Remenska <remenska@gmail.com>
2016-02-10 15:53:35 +0100

    Update post-link.sh

--- root5/post-link.sh
+++ root5/post-link.sh
@@ -3,6 +3,7 @@ source ${PREFIX}/bin/thisroot.sh
 if [-n ${CONDA_ENV_PATH} ]; then
      source ${CONDA_ENV_PATH}/bin/thisroot.sh # in case already active in some environment
 fi
+
 echo "Postlink: ROOT has been sourced. Environment settings are ready. "
 echo "ROOTSYS="${ROOTSYS}
 
