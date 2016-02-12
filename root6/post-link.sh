76b8394da57f1905bea1dd24ae2c3a898fb66c60
Daniela Remenska <remenska@gmail.com>
2016-02-12 15:27:08 +0100

    Update post-link.sh

--- root6/post-link.sh
+++ root6/post-link.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 source $PREFIX/bin/thisroot.sh
 
-echo "Postlink: ROOT has been sourced. Environment settings are ready. "
-echo "ROOTSYS="${ROOTSYS}
+#echo "Postlink: ROOT has been sourced. Environment settings are ready. "
+#echo "ROOTSYS="${ROOTSYS}
+if [ -z "${ROOTSYS}" ]; then
+     echo "Please set the following environment variables:"
+     echo "export ROOTSYS=$PREFIX"
+     echo "export PYTHONPATH=$PREFIX/lib"
+fi
 
 if [ -n "${LD_LIBRARY_PATH}" ]; then
      unset LD_LIBRARY_PATH
