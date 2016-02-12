5403775f9438f7ba398ea3aba8a8960e11ba8ce6
Daniela Remenska <remenska@gmail.com>
2016-02-12 10:02:50 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,4 @@
 cd /code
 CONDA_ENVS_PATH=$PWD/envs CONDA_BLD_PATH=$PWD/conda-bld conda build CONDA_PY=$CONDA_PY ./root$ROOT should_upload=$?
 chown -R $MYUID .
-if [ "should_upload" -eq 0 ]; then
-  if [ -n "$1" ]
-    then
-      echo "Going to upload the binary..."
-      anaconda upload $PWD/conda-bld/*/root-$ROOT*.tar.bz2
-    else
-      echo "Not uploading binary..."
-  fi
-else
-    echo FAIL
-fi
+anaconda upload $PWD/conda-bld/*/root-$ROOT*.tar.bz2 --force
