f19ea29c2124c1db4624736d39b3f73d487084d2
Daniela Remenska <remenska@gmail.com>
2016-02-12 13:05:19 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 
 cd /code
 CONDA_ENVS_PATH=$PWD/envs CONDA_BLD_PATH=$PWD/conda-bld conda build CONDA_PY=$CONDA_PY ./root$ROOT
-chown -R $MYUID .
-anaconda upload $PWD/conda-bld/*/root-$ROOT*.tar.bz2 --force --channel test
+retval=$?
+if [ $retval -eq 0 ]; then
+    echo "Uploading to test channel...."
+    chown -R $MYUID .
+    anaconda upload $PWD/conda-bld/*/root-$ROOT*.tar.bz2 --force --channel test
+else
+    echo "Building failed."
+fi
