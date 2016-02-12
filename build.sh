f30b739a8a4220d1a72e79f0fc2c9290762c458a
Daniela Remenska <remenska@gmail.com>
2016-02-12 09:25:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 cd /code
 CONDA_ENVS_PATH=$PWD/envs CONDA_BLD_PATH=$PWD/conda-bld conda build CONDA_PY=$CONDA_PY ./root$ROOT should_upload=$?
 chown -R $MYUID .
-if [ "$build_result" -eq 0 ]; then
+if [ "should_upload" -eq 0 ]; then
   if [ -n "$1" ]
     then
       echo "Going to upload the binary..."
