21afad93fc4f18f47e69f7d2fbb04294fce09c4d
Daniela Remenska <remenska@gmail.com>
2016-02-19 13:34:10 +0100

    Update build.sh
    Include Anaconda token.

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-cd /code
-CONDA_ENVS_PATH=$PWD/envs CONDA_BLD_PATH=$PWD/conda-bld conda build CONDA_PY=$CONDA_PY ./root$ROOT
-retval=$?
-if [ $retval -eq 0 ]; then
-    echo "Uploading to test channel...."
-    chown -R $MYUID .
-    anaconda upload $PWD/conda-bld/*/root-$ROOT*.tar.bz2 --force --channel test
+ANACONDA_TOKEN=$1
+
+if [ $# -eq 0 ]; then
+    echo "No token supplied for Anaconda upload."
 else
+    cd /code
+    CONDA_ENVS_PATH=$PWD/envs CONDA_BLD_PATH=$PWD/conda-bld conda build CONDA_PY=$CONDA_PY ./root$ROOT
+    retval=$?
+    if [ $retval -eq 0 ]; then
+        echo "Uploading to test channel...."
+        chown -R $MYUID .
+        anaconda -t $ANACONDA_TOKEN upload $PWD/conda-bld/*/root-$ROOT*.tar.bz2 --force --channel test
+    else
     echo "Building failed."
+    fi
 fi
