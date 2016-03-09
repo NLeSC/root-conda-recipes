3636207d96b2f38938e9955194a8906833f3005b
Daniela Remenska <remenska@gmail.com>
2016-03-09 15:13:48 +0100

    Update build.sh
    channel name is dev instead of test

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ else
     if [ $retval -eq 0 ]; then
         echo "Uploading to Anaconda cloud, test channel...."
         chown -R $MYUID .
-        anaconda -v -t $ANACONDA_TOKEN upload $PWD/conda-bld/*/root-$ROOT*.tar.bz2 --force --channel test
+        anaconda -v -t $ANACONDA_TOKEN upload $PWD/conda-bld/*/root-$ROOT*.tar.bz2 --force --channel dev
     else
         echo "Building failed."
         exit $retval
