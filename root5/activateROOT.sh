50420ca8fea203c03a66151436c2f24fe3299ffb
Faruk D <f.diblen@esciencecenter.nl>
2018-05-09 15:52:34 +0200

    add new OSX build options and cleanup

--- root5/activateROOT.sh
+++ root5/activateROOT.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-#echo ${CONDA_ENV_PATH}
-#source ${CONDA_ENV_PATH}/bin/thisroot.sh
+
 source $(which thisroot.sh)
 
-echo "Activate: ROOT has been sourced. Environment settings are ready. "
+echo "Activate: ROOT has been configured. Environment settings are ready."
 echo "ROOTSYS="${ROOTSYS}
 
 if [ -n "${LD_LIBRARY_PATH}" ]; then
      unset LD_LIBRARY_PATH
 fi
 
-
 if [ -n "${DYLD_LIBRARY_PATH}" ]; then
      unset DYLD_LIBRARY_PATH
 fi
-
 
--- root5/deactivateROOT.sh
+++ root5/deactivateROOT.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Deactivate:Unsetting ROOT environment variables.."
+
+echo "Deactivate:Unsetting ROOT environment variables..."
 
 if [ -n "${ROOTSYS}" ]; then
      unset ROOTSYS
@@ -9,13 +10,10 @@ if [ -n "${PYTHONPATH}" ]; then
      unset PYTHONPATH
 fi
 
-
 if [ -n "${LD_LIBRARY_PATH}" ]; then
      unset LD_LIBRARY_PATH
 fi
 
-
 if [ -n "${DYLD_LIBRARY_PATH}" ]; then
      unset DYLD_LIBRARY_PATH
 fi
-
