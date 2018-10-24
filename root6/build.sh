0ca1ca821ae37121ce791b1f93ead3ac08e39e3b
Faruk D <f.diblen@esciencecenter.nl>
2018-10-24 23:15:01 +0200

    build with gcc7

--- root6/build.sh
+++ root6/build.sh
@@ -53,6 +53,7 @@ LinuxInstallation() {
     echo "PWD: " $PWD
 
     cmake $CONDA_WORKDIR/ -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 \
         -Dbuiltin_afterimage=OFF \
         -Dbuiltin_ftgl=OFF \
         -Dbuiltin_fftw3=ON \
