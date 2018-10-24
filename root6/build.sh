37420c69ae8eec1cd86e3fdd4ed9f6ae1cfc430e
Faruk D <f.diblen@esciencecenter.nl>
2018-10-24 23:28:39 +0200

    cache disabled to solve environment variable problems

--- root6/build.sh
+++ root6/build.sh
@@ -53,7 +53,6 @@ LinuxInstallation() {
     echo "PWD: " $PWD
 
     cmake $CONDA_WORKDIR/ -DCMAKE_INSTALL_PREFIX=$PREFIX \
-        -DCMAKE_C_COMPILER=/usr/bin/gcc-7 -DCMAKE_CXX_COMPILER=/usr/bin/g++-7 \
         -Dbuiltin_afterimage=OFF \
         -Dbuiltin_ftgl=OFF \
         -Dbuiltin_fftw3=ON \
